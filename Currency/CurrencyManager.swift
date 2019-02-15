b34a62c5167901af4c63bcabc295eca8883ff7dc
Andy Liang <meteochu@gmail.com>
2019-02-15 01:16:11 -0500

    Add API Key

--- Currency/CurrencyManager.swift
+++ Currency/CurrencyManager.swift
@@ -8,11 +8,12 @@
 
 import Foundation
 
+private let apiKey = "e79aa1dc3e084c32b6ea"
 struct CurrencyManager {
 	
 	static let shared = CurrencyManager()
 	
-	private static let requestUrl = URL(string: "https://free.currencyconverterapi.com/api/v5/convert?q=USD_CAD&compact=y")!
+	private static let requestUrl = URL(string: "https://free.currencyconverterapi.com/api/v5/convert?q=USD_CAD&compact=y&apiKey=\(apiKey)")!
 	
 	func refresh(completion: @escaping (CurrencyResult?) -> Void) {
 		let dataTask = URLSession.shared.dataTask(with: CurrencyManager.requestUrl) { data, response, error in
